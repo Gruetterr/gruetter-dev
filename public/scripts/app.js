90f9873afc8f4d6338a53ee23673100444fbf489
Adrian <adrian.worm@icloud.com>
2024-10-19 22:09:12 +0200

    THIS GOTTA BE IT!!!

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -109,7 +109,7 @@ async function rsaDe() {
   for (let i = 0; i < de_blocks; i++) {
     // Get current block
     if (i === de_blocks - 1) {
-      cur_block = c_str.substring(i * (N_str.length - 1), c_str.length - 3);
+      cur_block = c_str.substring(i * N_str.length, c_str.length - 3);
     } else {
       //cur_block = c_str.substring(i * (N_str.length - 1) + 3, (i + 1) * (N_str.length - 1) + 3);
       cur_block = c_str.substr(i * N_str.length, N_str.length);
